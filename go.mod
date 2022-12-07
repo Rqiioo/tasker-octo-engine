ee19bacf5c63bd0c3d6a4a6aeaa7cfadb4357f72
Gucci <116963289+Rqiioo@users.noreply.github.com>
2022-12-07 00:47:24 -0600

    Initialize new-logo

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/go-task/task/v3
+
+require (
+	github.com/fatih/color v1.7.0
+	github.com/go-task/slim-sprig v0.0.0-20200516131648-f9bac4e523eb
+	github.com/joho/godotenv v1.3.0
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-zglob v0.0.1
+	github.com/radovskyb/watcher v1.0.5
+	github.com/spf13/pflag v1.0.3
+	github.com/stretchr/testify v1.5.1
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
+	mvdan.cc/sh/v3 v3.2.1
+)
+
+go 1.13
---
+++ init.go
@@ -0,0 +1,38 @@
+package task
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+const defaultTaskfile = `# https://taskfile.dev
+
+version: '3'
+
+vars:
+  GREETING: Hello, World!
+
+tasks:
+  default:
+    cmds:
+      - echo "{{.GREETING}}"
+    silent: true
+`
+
+// InitTaskfile Taskfile creates a new Taskfile
+func InitTaskfile(w io.Writer, dir string) error {
+	f := filepath.Join(dir, "Taskfile.yml")
+
+	if _, err := os.Stat(f); err == nil {
+		return ErrTaskfileAlreadyExists
+	}
+
+	if err := ioutil.WriteFile(f, []byte(defaultTaskfile), 0644); err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "Taskfile.yml created in the current directory\n")
+	return nil
+}
